0320a1d6c0156de7b4158b7d708d8bffcf1f11b3
christianeyee <christiane.yee@gmail.com>
2015-06-01 09:38:24 +0800

    Cleanup

--- Main.java
+++ Main.java
@@ -107,40 +107,4 @@ public class Main {
 			dataset.add(node);
 		}
 	}
-
-	/*
-	private static void buildData(String line) {
-		String t[] = line.split(",");
-		Data data = new Data(t[0], t[1], t[2], t[3], t[4], t[5], t[6],
-							 t[7], t[8], t[9], t[10], t[11], t[12], t[13], 
-							 t[14], t[15], t[16], t[17], t[18], t[19], t[20],
-							 t[21], t[22], t[23], t[24], t[25], t[26], t[27],
-							 t[28], t[29], t[30], t[41]);
-		dataset.add(data);
-		//Mapper.map(data);
-	}
-
-	private static void insertAll() {	
-		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("fill_table.sql"), "utf-8"))) {
-			String t;
-
-			for (Data temp : dataset) {
-				t = "INSERT INTO dataset VALUES (" + temp.duration + ", '"  + temp.protocol + "', '" + temp.service + "', '"
-				+ temp.flag + "', " + temp.src_bytes + ", " + temp.dst_bytes + ", " + temp.land + ", " + temp.wrong_fragment + ", "
-				+ temp.urgent + ", " + temp.hot + ", " + temp.num_failed_logins + ", " + temp.logged_in + ", "
-				+ temp.num_compromised + ", " + temp.root_shell + ", " + temp.su_attempted + ", " + temp.num_root + ", "
-				+ temp.num_file_creations + ", " + temp.num_shells + ", " + temp.num_access_files + ", " + temp.num_outbound_cmds + ", "
-				+ temp.is_host_login + ", " + temp.is_guest_login + ", " + temp.count + ", " + temp.srv_count + ", "
-				+ temp.serror_rate + ", " + temp.srv_serror_rate + ", " + temp.rerror_rate + ", " + temp.srv_rerror_rate + ", "
-				+ temp.same_srv_rate + ", " + temp.diff_srv_rate + ", " + temp.srv_diff_host_rate + ", '" + temp.classification + "');";
-				
-				writer.write("\n");
-				writer.write(t);
-			}
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	*/
 }
--- Mapper.java
+++ Mapper.java
@@ -3,28 +3,9 @@ import java.util.HashSet;
 
 public class Mapper {
 
-	//static HashSet<Integer> duration = new HashSet<Integer>();
 	static HashSet<String> protocol = new HashSet<String>();
-	//static HashSet<String> service = new HashSet<String>();
-	//static HashSet<String> flag = new HashSet<String>();
 	static HashSet<Integer> src_bytes = new HashSet<Integer>();
 	static HashSet<Integer> dst_bytes = new HashSet<Integer>();
-	//static HashSet<Short> land = new HashSet<Short>();
-	//static HashSet<Integer> wrong_fragment = new HashSet<Integer>();
-	//static HashSet<Integer> urgent = new HashSet<Integer>();
-	//static HashSet<Integer> hot = new HashSet<Integer>();
-	//static HashSet<Integer> num_failed_logins = new HashSet<Integer>();
-	//static HashSet<Short> logged_in = new HashSet<Short>();
-	//static HashSet<Integer> num_compromised = new HashSet<Integer>();
-	//static HashSet<Integer> root_shell = new HashSet<Integer>();
-	//static HashSet<Integer> su_attempted = new HashSet<Integer>();
-	//static HashSet<Integer> num_root = new HashSet<Integer>();
-	//static HashSet<Integer> num_file_creations = new HashSet<Integer>();
-	//static HashSet<Integer> num_shells = new HashSet<Integer>();
-	//static HashSet<Integer> num_access_files = new HashSet<Integer>();
-	//static HashSet<Integer> num_outbound_cmds = new HashSet<Integer>();
-	//static HashSet<Short> is_host_login = new HashSet<Short>();
-	//static HashSet<Short> is_guest_login = new HashSet<Short>();
 	static HashSet<Integer> count = new HashSet<Integer>();
 	static HashSet<Integer> srv_count = new HashSet<Integer>();
 	static HashSet<Float> serror_rate = new HashSet<Float>();
@@ -37,28 +18,9 @@ public class Mapper {
 	static HashSet<String> classification = new HashSet<String>();
 
 	public static void map(Data data) {
-		//duration.add(data.duration);
 		protocol.add(data.protocol);
-		//service.add(data.service);
-		//flag.add(data.flag);
 		src_bytes.add(data.src_bytes);
 		dst_bytes.add(data.dst_bytes);
-		//land.add(data.land);
-		//wrong_fragment.add(data.wrong_fragment);
-		//urgent.add(data.urgent);
-		//hot.add(data.hot);
-		//num_failed_logins.add(data.num_failed_logins);
-		//logged_in.add(data.logged_in);
-		//num_compromised.add(data.num_compromised);
-		//root_shell.add(data.root_shell);
-		//su_attempted.add(data.su_attempted);
-		//num_root.add(data.num_root);
-		//num_file_creations.add(data.num_file_creations);
-		//num_shells.add(data.num_shells);
-		//num_access_files.add(data.num_access_files);
-		//num_outbound_cmds.add(data.num_outbound_cmds);
-		//is_host_login.add(data.is_host_login);
-		//is_guest_login.add(data.is_guest_login);
 		count.add(data.count);
 		srv_count.add(data.srv_count);
 		serror_rate.add(data.serror_rate);
@@ -83,136 +45,50 @@ public class Mapper {
 		
 		System.out.println();
 		System.out.println("protocol");
-		//System.out.println(protocol);
+		System.out.println(protocol);
 
 		System.out.println();
 		System.out.println("src_bytes");
-		//System.out.println(src_bytes);
-		System.out.println(Collections.max(src_bytes));
+		System.out.println(src_bytes);
 
 		System.out.println();
 		System.out.println("dst_bytes");
-		//System.out.println(dst_bytes);
-		System.out.println(Collections.max(dst_bytes));
+		System.out.println(dst_bytes);
 		
-		/*
-		System.out.println();
-		System.out.println("duration");
-		//System.out.println(duration);
-		System.out.println(Collections.max(duration));
-
-		System.out.println();
-		System.out.println("service");
-		//System.out.println(flag);
-
-		System.out.println();
-		System.out.println("land");
-		//System.out.println(land);
-
-		System.out.println();
-		System.out.println("wrong_fragment");
-		//System.out.println(wrong_fragment);
-		System.out.println(Collections.max(wrong_fragment));
-
-		System.out.println();
-		System.out.println("urgent");
-		//System.out.println(urgent);
-		System.out.println(Collections.max(urgent));
-
-		System.out.println();
-		System.out.println("hot");
-		//System.out.println(hot);
-		System.out.println(Collections.max(urgent));
-
-		System.out.println();
-		System.out.println("num_failed_logins");
-		//System.out.println(num_failed_logins);
-		System.out.println(Collections.max(num_failed_logins));
-
-		System.out.println();
-		System.out.println("logged_in");
-		//System.out.println(num_compromised);
-
-		System.out.println();
-		System.out.println("root_shell");
-		//System.out.println(root_shell);
-		System.out.println(Collections.max(root_shell));
-
-		System.out.println();
-		System.out.println("su_attempted");
-		//System.out.println(su_attempted);
-		System.out.println(Collections.max(su_attempted));
-
-		System.out.println();
-		System.out.println("num_root");
-		//System.out.println(num_root);
-		System.out.println(Collections.max(num_root));
-
-		System.out.println();
-		System.out.println("num_file_creations");
-		//System.out.println(num_file_creations);
-		System.out.println(Collections.max(num_file_creations));
-
-		System.out.println();
-		System.out.println("num_shells");
-		//System.out.println(num_shells);
-		System.out.println(Collections.max(num_shells));
-
-		System.out.println();
-		System.out.println("num_access_files");
-		//System.out.println(num_access_files);
-		System.out.println(Collections.max(num_access_files));
-
-		System.out.println();
-		System.out.println("num_outbound_cmds");
-		//System.out.println(num_outbound_cmds);
-		System.out.println(Collections.max(num_outbound_cmds));
-
-		System.out.println();
-		System.out.println("is_host_login");
-		//System.out.println(is_host_login);
-
-		System.out.println();
-		System.out.println("is_guest_login");
-		//System.out.println(is_guest_login);
-		*/
-
 		System.out.println();
 		System.out.println("count");
-		//System.out.println(count);
-		System.out.println(Collections.max(count));
+		System.out.println(count);
 
 		System.out.println();
 		System.out.println("srv_count");
-		//System.out.println(srv_count);
-		System.out.println(Collections.max(srv_count));
+		System.out.println(srv_count);
 
 		System.out.println();
 		System.out.println("serror_rate");
-		//System.out.println(serror_rate);
+		System.out.println(serror_rate);
 
 		System.out.println();
 		System.out.println("srv_serror_rate");
-		//System.out.println(srv_serror_rate);
+		System.out.println(srv_serror_rate);
 
 		System.out.println();
 		System.out.println("rerror_rate");
-		//System.out.println(rerror_rate);
+		System.out.println(rerror_rate);
 
 		System.out.println();
 		System.out.println("srv_rerror_rate");
-		//System.out.println(srv_rerror_rate);
+		System.out.println(srv_rerror_rate);
 
 		System.out.println();
 		System.out.println("same_srv_rate");
-		//System.out.println(serror_rate);
+		System.out.println(serror_rate);
 
 		System.out.println();
 		System.out.println("diff_srv_rate");
-		//System.out.println(serror_rate);
+		System.out.println(serror_rate);
 
 		System.out.println();
 		System.out.println("srv_diff_host_rate");
-		//System.out.println(serror_rate);
+		System.out.println(serror_rate);
 	}
 }
